4e4ab4c9f02095125e24b55eaf41a6787a4f4eb3
yinmh17 <31975170+yinmh17@users.noreply.github.com>
2019-10-21 02:13:15 +0800

    Update and rename run_fs_pspnet_cityscapes_seg.sh to run_fs_res101_pspnet_cityscapes_seg.sh

--- scripts/seg/cityscapes/run_fs_res101_pspnet_cityscapes_seg.sh
+++ scripts/seg/cityscapes/run_fs_res101_pspnet_cityscapes_seg.sh
@@ -16,7 +16,7 @@ CHECKPOINTS_NAME="fs_pspnet_cityscapes_seg"$2
 PRETRAINED_MODEL="./pretrained_models/3x3resnet101-imagenet.pth"
 
 CONFIG_FILE='configs/seg/cityscapes/base_fcn_cityscapes_seg.conf'
-MAX_ITERS=40000
+MAX_ITERS=20000
 LOSS_TYPE="dsnce_loss"
 
 LOG_DIR="./log/seg/cityscapes/"
