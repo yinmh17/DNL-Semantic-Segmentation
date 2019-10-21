16b5199e04481c4e681b86be2072cda905037d53
yinmh17 <31975170+yinmh17@users.noreply.github.com>
2019-10-22 03:28:14 +0800

    Update run_fs_res101_pspnet_cityscapes_seg.sh

--- scripts/seg/cityscapes/run_fs_res101_pspnet_cityscapes_seg.sh
+++ scripts/seg/cityscapes/run_fs_res101_pspnet_cityscapes_seg.sh
@@ -8,15 +8,15 @@ WORK_DIR=$(cd $(dirname $0)/../../../;pwd)
 export PYTHONPATH=${WORK_DIR}:${PYTHONPATH}
 cd ${WORK_DIR}
 
-DATA_DIR="//philly/rr1/public/v-miyin/data/cityscapes.zip@"
+DATA_DIR="//philly/eu2/resrchvc/v-miyin/data/cityscapes.zip@"
 
 BACKBONE="deepbase_resnet101_dilated8"
 MODEL_NAME="pspnet"
 CHECKPOINTS_NAME="fs_pspnet_cityscapes_seg"$2
-PRETRAINED_MODEL="//philly/rr1/public/v-miyin/model/resnet101-imagenet.pth"
+PRETRAINED_MODEL="//philly/eu2/resrchvc/v-miyin/model/resnet101-imagenet.pth"
 
 CONFIG_FILE='configs/seg/cityscapes/base_fcn_cityscapes_seg.conf'
-MAX_ITERS=40000
+MAX_ITERS=20000
 LOSS_TYPE="dsnce_loss"
 
 LOG_DIR="./log/seg/cityscapes/"
@@ -30,7 +30,7 @@ fi
 export NCCL_LL_THRESHOLD=0
 export NCCL_TREE_THRESHOLD=0
 
-NGPUS=4
+NGPUS=8
 DIST_PYTHON="${PYTHON} -u -m torch.distributed.launch --nproc_per_node=${NGPUS}"
 
 if [[ "$1"x == "train"x ]]; then
