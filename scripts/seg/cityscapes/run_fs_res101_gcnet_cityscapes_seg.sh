35c22d2f33d1a9fdc5fcd29e02061e159d6baa2b
yinmh17 <31975170+yinmh17@users.noreply.github.com>
2019-10-21 14:58:09 +0800

    Update run_fs_res101_gcnet_cityscapes_seg.sh

--- scripts/seg/cityscapes/run_fs_res101_gcnet_cityscapes_seg.sh
+++ scripts/seg/cityscapes/run_fs_res101_gcnet_cityscapes_seg.sh
@@ -24,6 +24,8 @@ if [[ ! -d ${LOG_DIR} ]]; then
     mkdir -p ${LOG_DIR}
 fi
 export NCCL_LL_THRESHOLD=0
+export NCCL_TREE_THRESHOLD=0
+
 NGPUS=8
 DIST_PYTHON="${PYTHON} -m torch.distributed.launch --nproc_per_node=${NGPUS}"
 if [[ "$1"x == "train"x ]]; then
