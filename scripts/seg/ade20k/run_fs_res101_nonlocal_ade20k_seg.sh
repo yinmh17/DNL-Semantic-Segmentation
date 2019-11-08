7d5704ed589725d4648d353a1ef49c682114422e
yinmh17 <31975170+yinmh17@users.noreply.github.com>
2019-11-08 23:33:13 +0800

    Update run_fs_res101_nonlocal_ade20k_seg.sh

--- scripts/seg/ade20k/run_fs_res101_nonlocal_ade20k_seg.sh
+++ scripts/seg/ade20k/run_fs_res101_nonlocal_ade20k_seg.sh
@@ -8,17 +8,17 @@ WORK_DIR=$(cd $(dirname $0)/../../../;pwd)
 export PYTHONPATH=${WORK_DIR}:${PYTHONPATH}
 cd ${WORK_DIR}
 
-DATA_DIR="//philly/rr1/public/v-miyin/data/ADE20K.zip@"
+DATA_DIR="/data/home/v-miyin/ADE20K"
 
 BACKBONE="deepbase_resnet101_dilated8"
 MODEL_NAME="nonlocalbn"
 CHECKPOINTS_NAME="fs_res101_nonlocal_ade20k_seg"$2
-PRETRAINED_MODEL="//philly/rr1/public/v-miyin/model/resnet101-imagenet.pth"
+PRETRAINED_MODEL="./pretrained_models/3x3resnet101-imagenet.pth"
 
 CONFIG_FILE='configs/seg/ade20k/NL_fcn_ade20k_seg.conf'
-#MAX_ITERS=150000
-MAX_ITERS=75000
-LOSS_TYPE="dsnce_loss"
+MAX_ITERS=150000
+#MAX_ITERS=75000
+LOSS_TYPE="dsnohemce_loss"
 
 LOG_DIR="./log/seg/ade20k/"
 LOG_FILE="${LOG_DIR}${CHECKPOINTS_NAME}.log"
@@ -29,28 +29,28 @@ if [[ ! -d ${LOG_DIR} ]]; then
 fi
 
 export NCCL_LL_THRESHOLD=0
-export NCCL_TREE_THRESHOLD=0
+#export NCCL_TREE_THRESHOLD=0
 
 NGPUS=8
 DIST_PYTHON="${PYTHON} -m torch.distributed.launch --nproc_per_node=${NGPUS}"
 
 if [[ "$1"x == "train"x ]]; then
-  ${DIST_PYTHON} main.py --config_file ${CONFIG_FILE} --phase train --train_batch_size 1 --val_batch_size 1 \
-                         --backbone ${BACKBONE} --model_name ${MODEL_NAME} --drop_last y --syncbn y --dist y \
+  ${DIST_PYTHON} main.py --config_file ${CONFIG_FILE} --phase train --train_batch_size 2 --val_batch_size 1 \
+                         --backbone ${BACKBONE} --model_name ${MODEL_NAME} --gpu 0 1 2 3 4 5 6 7 --drop_last y --syncbn y --dist y \
                          --data_dir ${DATA_DIR} --loss_type ${LOSS_TYPE} --max_iters ${MAX_ITERS} \
                          --checkpoints_name ${CHECKPOINTS_NAME} --pretrained ${PRETRAINED_MODEL}  2>&1 | tee ${LOG_FILE}
 
 elif [[ "$1"x == "resume"x ]]; then
-  ${DIST_PYTHON} main.py --config_file ${CONFIG_FILE} --phase train --train_batch_size 1 --val_batch_size 1 \
+  ${DIST_PYTHON} main.py --config_file ${CONFIG_FILE} --phase train --train_batch_size 2 --val_batch_size 1 \
                          --backbone ${BACKBONE} --model_name ${MODEL_NAME} --drop_last y --syncbn y --dist y \
                          --data_dir ${DATA_DIR} --loss_type ${LOSS_TYPE} --max_iters ${MAX_ITERS} \
                          --resume_continue y --resume ./checkpoints/seg/ade20k/${CHECKPOINTS_NAME}_latest.pth \
                          --checkpoints_name ${CHECKPOINTS_NAME} --pretrained ${PRETRAINED_MODEL}  2>&1 | tee -a ${LOG_FILE}
 
 elif [[ "$1"x == "val"x ]]; then
-  ${PYTHON} main.py --config_file ${CONFIG_FILE} --phase test --gpu 0 1 2 3 --gather n \
+  ${PYTHON} main.py --config_file ${CONFIG_FILE} --phase test --gpu 0 1 2 3  --gather n \
                     --backbone ${BACKBONE} --model_name ${MODEL_NAME} --checkpoints_name ${CHECKPOINTS_NAME} \
-                    --resume ./checkpoints/seg/ade20k/${CHECKPOINTS_NAME}_latest.pth \
+                    --resume ./checkpoints/seg/ade20k/${CHECKPOINTS_NAME}_latest.pth  --resume_strict y\
                     --test_dir ${DATA_DIR}/val/image --out_dir val  2>&1 | tee -a ${LOG_FILE}
   cd metric/seg/
   ${PYTHON} seg_evaluator.py --config_file "../../"${CONFIG_FILE} \
